393c770e3448ae0a0440fca44ea823e9c8456a15
Patricio Lopez <pelopez2@uc.cl>
2014-10-26 11:32:20 -0300

    missing class

---
+++ WPF/Espacio_v2/Backend/Subclasese ObjetoEspacial/Estrella.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    class Estrella : ObjetoEspacial
+    {
+        private double Constante { get; set; }
+        private double Pendiente { get; set; }
+
+        public override string NombreImagen
+        {
+            get { return "star.png"; }
+        }
+
+        public Estrella(Random generador, double X, double Y) : base(X, Y)
+        {
+            Constante = generador.Next(-10, 10) * 0.0001;
+            Pendiente = generador.Next(-100, 100) * 0.00001;
+            W = H = generador.Next(5, 30);
+        }
+
+        public override void Moverse(double cantidad)
+        {
+            X -= cantidad;
+            Y += Pendiente * cantidad + Constante;
+
+            GatillarCambioCoordenadas(X, Y);
+        }
+    }
+}
